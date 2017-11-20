55a80f77eb0af0d56ddc474cd52ac6008cd56072
Delta Force <31012452+DeltaForx@users.noreply.github.com>
2017-11-20 18:10:58 +0530

    Create 04_A Very Big Sum.java

---
+++ Algorithms/01_Warmup/04_A Very Big Sum.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static long aVeryBigSum(int n, long[] ar) {
+        long sum = 0;
+        for(int i = 0; i< n;i++)
+        {
+            sum = sum + ar[i];
+        }
+        return sum;
+    }
+    
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        long[] ar = new long[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextLong();
+        }
+        long result = aVeryBigSum(n, ar);
+        System.out.println(result);
+    }
+}
