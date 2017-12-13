65ec30c941b502bfc3d0da3ed56ac2920d374bcb
Isha Mohan <ishamohano97@gmail.com>
2017-12-13 11:50:00 +0530

    Create Print in Reverse.java

---
+++ Data Structures/02_Linked Lists/06_Print in Reverse/Print in Reverse.java
@@ -0,0 +1,20 @@
+/*
+  Print elements of a linked list in reverse order 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+    // This is a "method-only" submission. 
+    // You only need to complete this method. 
+
+void ReversePrint(Node head) {
+  // This is a "method-only" submission. 
+  // You only need to complete this method. 
+    if(head == null)
+        return;
+    ReversePrint(head.next);
+    System.out.println(head.data);
+}
