fedd07bffb1b366df3e216e6c762648465389fea
Isha Mohan <ishamohano97@gmail.com>
2017-12-16 20:08:33 +0530

    Create Solution.java

---
+++ Data Structures/01_Arrays/04_Left Rotation/Solution.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] leftRotation(int[] a, int d) {
+        // Complete this function
+        int size = a.length;
+        for(int i=0;i<d;i++)
+            rotateByOne(a,size);
+        return a;
+    }
+    
+    static void rotateByOne(int[] arr,int size)
+    {
+        int temp = arr[0];
+        for(int i=0;i<size-1;i++)
+        {
+            arr[i]=arr[i+1];
+        }
+        arr[size-1] = temp;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int d = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i = 0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        int[] result = leftRotation(a, d);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+        }
+        System.out.println("");
+
+
+        in.close();
+    }
+}
